78088de5bb88a6a1d4203bfe24075141a89c3b08
Joe Nelson <github@begriffsschrift.com>
2012-02-02 23:50:52 -0600

    switched to Prover9

--- tests.py
+++
@@ -1,7 +0,0 @@
-import axioms
-import filler
-
-for i in range(3**(3*3)):
-        tablefunc = axioms.t2f(filler.ithNTable(2, i))
-        if axioms.check(range(2), tablefunc, tablefunc, axioms.semiquandle):
-                print i
