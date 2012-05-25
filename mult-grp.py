27b470dd2c3dfd5bc1f8c8110fe9f4e633e959f5
Joe Nelson <cred+github@begriffs.com>
2012-05-24 21:50:01 -0500

    more info about multiplicative groups

--- mult-grp.py
+++ mult-grp.py
@@ -15,14 +15,37 @@ def generate(perms):
       break
   return grp
 
+def table(M):
+  n = len(M)
+  M = list(M)
+  T = [[-1 for x in range(n)] for x in xrange(n)]
+  for i, a in enumerate(M):
+    for j, b in enumerate(M):
+      T[i][j] = M.index( funcs.compose(a,b) )
+  return T
+
+def isAbelian(M):
+  for a, b in itertools.combinations(M, 2):
+    if funcs.compose(a,b) != funcs.compose(b,a):
+      return False
+  return True
 
 ops = ladr_python.opsInFile(sys.stdin)
 i = 1
 for (down_table, up_table) in ops:
   up_grp = generate(map(tuple, zip(*up_table)))
   dn_grp = generate(map(tuple, zip(*down_table)))
+  print "Up:"
+  print ladr_python.matrixToLadr(table(up_grp), i)
+  print "Abelian=" + str(isAbelian(up_grp)) + ", cycle=" + str(max([len(generate([x])) for x in up_grp]))
+  print "Dn:"
+  print ladr_python.matrixToLadr(table(dn_grp), i)
+  print "Abelian=" + str(isAbelian(dn_grp)) + ", cycle=" + str(max([len(generate([x])) for x in dn_grp]))
+  print "Wh:"
   whole_grp = generate(up_grp.union(dn_grp))
-  print [sys.argv[1] + "_" + str(i), len(up_grp), len(dn_grp), len(whole_grp)]
+  print ladr_python.matrixToLadr(table(whole_grp), i)
+  print "Abelian=" + str(isAbelian(whole_grp)) + ", cycle=" + str(max([len(generate([x])) for x in whole_grp]))
+
+  #print ladr_python.matrixToLadr(table(whole_grp), i)
+  #print [sys.argv[1] + "_" + str(i), len(up_grp), len(dn_grp), len(whole_grp)]
   i += 1
-
-
